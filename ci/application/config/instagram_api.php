b519d22300dea9ef66ce03307dc5b73110b8b403
Kamena <kamena.dacheva@gmail.com>
2015-04-05 22:00:42 +0300

    Now we can see the notifications from facebook_process.php in admin_page.php. Also they are in slider. (Yay!)

--- ci/application/config/instagram_api.php
+++ ci/application/config/instagram_api.php
@@ -12,8 +12,8 @@
 $config['instagram_client_name']	= 'SocialPlaza';
 $config['instagram_client_id']		= '139f4c82603643519941e15eb3e026ea';
 $config['instagram_client_secret']	= '471bc0a7a75b4a46ba704d6834ef0d60';
-$config['instagram_callback_url']	= 'http://socialplaza.info/ci/Authorize/get_code';
-$config['instagram_website']		= 'http://socialplaza.info';
+$config['instagram_callback_url']	= 'http://localhost/ci/authorize/get_code';
+$config['instagram_website']		= 'http://localhost';
 $config['instagram_description']	= '';
 	
 /**
